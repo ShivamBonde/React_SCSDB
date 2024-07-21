8c83d270f86220d92396eab01581a74655c15807
Shivam Bonde <shivambonde19@gmail.com>
2024-07-21 21:26:15 +0530

    horizontal component added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,34 +3,60 @@ import Sidenav from "./partials/Sidenav";
 import Topnav from "./partials/Topnav";
 import axios from "../utils/axios";
 import Header from "./partials/Header";
+import HorizontalCards from "./partials/HorizontalCards";
+import Dropdown from "./partials/Dropdown";
 
 const Home = () => {
   document.title = "SCSDB | Homepage";
   const [wallpaper, setwallpaper] = useState(null);
+  const [trending, setTrending] = useState(null);
+  const [category, setCategory] = useState("all");
 
-  const GetHeaderWallpaper=async () => {
+  const GetHeaderWallpaper = async () => {
     try {
       const { data } = await axios.get(`/trending/all/day`);
-      let randomdata =data.results[(Math.random()*data.results.length).toFixed()];
+      let randomdata =
+        data.results[(Math.random() * data.results.length).toFixed()];
       setwallpaper(randomdata);
     } catch (error) {
       console.error("Error:", error);
     }
   };
-  
 
-  useEffect(()=>{
-    !wallpaper &&  GetHeaderWallpaper();
-  })
-  return wallpaper?(
+  const GetTrending = async () => {
+    try {
+      const { data } = await axios.get(`/trending/${category}/day`);
+      setTrending(data.results);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    GetTrending();
+    !wallpaper && GetHeaderWallpaper();
+  }, [category]);
+  return wallpaper && trending ? (
     <>
       <Sidenav />
-      <div className="w-[80%] h-full">
+      <div className="w-[80%] h-full overflow-auto overflow-x-hidden">
         <Topnav />
-        <Header data={wallpaper}/>
+        <Header data={wallpaper} />
+
+        <div className="mt-5 flex justify-between p-3">
+          <h1 className=" text-3xl text-zinc-400 font-semibold">Trending</h1>
+          <Dropdown
+            title="Filter"
+            options={["tv", "movie", "all"]}
+            func={(e) => setCategory(e.target.value)}
+          />
+        </div>
+        <HorizontalCards data={trending} />
       </div>
     </>
-  ):<h1>normal loading</h1>;
+  ) : (
+    <h1>normal loading</h1>
+  );
 };
 
 export default Home;
---
+++ src/components/partials/HorizontalCards.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Dropdown from "./Dropdown";
+function HorizontalCards({ data, func }) {
+  return (
+    <div className="w-[100%] flex overflow-y-hidden mb-5 p-5">
+      {data.map((d, i) => {
+        return (
+          <div key={i} className="min-w-[15%] bg-zinc-900 mr-5 mb-5">
+            <img
+              className="w-full h-[55%] object-cover"
+              src={`https://image.tmdb.org/t/p/original${
+                d.backdrop_path || d.poster_path 
+              }`}
+              alt=""
+            />
+            <div className="text-white p-3 h-[45%]">
+              <h1 className="text-xl font-semibold ">
+                {d.name || d.title || d.original_name || d.original_title}
+              </h1>
+              <p className="text-sm">
+                {d.overview.slice(0, 50)}...
+                <span className="text-zinc-400">more</span>
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+export default HorizontalCards;
